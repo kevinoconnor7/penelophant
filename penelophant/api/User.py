c1138409f406ea7dd4e818ec86073d773e3cac20
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-03-06 16:55:08 -0500

    Complete the User API endpoint

--- penelophant/api/User.py
+++ penelophant/api/User.py
@@ -1,5 +1,6 @@
 """ User API representation object """
 
+from sqlalchemy.exc import IntegrityError
 from flask_restful import Resource, abort, reqparse
 from penelophant.models.User import User as User_model
 from penelophant.database import db
@@ -14,13 +15,25 @@ class User(Resource):
   def get(self, user_id):
     """ Retrieve user """
     user = self.get_user_by_id_or_abort(user_id)
-    return user.to_api()
+    return user.to_api(), 200
 
   def delete(self, user_id):
     """ Delete user """
     user = self.get_user_by_id_or_abort(user_id)
     crud.delete(user)
     return '', 204
+
+  def put(self, user_id):
+    """ Update a user """
+    parser = reqparse.RequestParser()
+    parser.add_argument('email', type=str)
+    args = parser.parse_args()
+
+    user = self.get_user_by_id_or_abort(user_id)
+    user.email = args.email
+    crud.save()
+
+    return user.to_api(), 200
 
   def get_user_by_id_or_abort(self, user_id):
     """ Attempt to get user by id, or abort if it goes south """
@@ -46,7 +59,10 @@ class UserList(Resource):
     user = User_model()
     user.email = args.email
 
-    crud.add(user)
+    try:
+      crud.add(user)
+    except IntegrityError:
+      abort(400, message="User %s already exists" % args.email)
     return user.to_api(), 201
 
   def get(self):
