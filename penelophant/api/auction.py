8d52f4aaa40694fcc2193af47af167f4901c8e8b
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-03-31 23:28:01 -0400

    Add api endpoint to view a list of auctions

--- penelophant/api/auction.py
+++ penelophant/api/auction.py
@@ -1,14 +1,28 @@
 """ Auction REST resources """
 
+from flask import g
 from flask_restful import Resource, reqparse
 from decimal import Decimal
-from penelophant import crud
+from penelophant import crud, auther
+from penelophant.database import db
 from penelophant.helpers.auction import find_auction_type
+from penelophant.models.Auction import Auction as Auction_model
 
 class AuctionList(Resource):
   """ Auction List REST API """
 
-  #@auther.login_required
+  def get(self):
+    """ List all auctions """
+    session = db.session
+    auctions = session.query(Auction_model).all()
+
+    data = dict()
+    data['auctions'] = [auction.to_api() for auction in auctions]
+    data['length'] = len(auctions)
+
+    return data, 200
+
+  @auther.login_required
   def post(self):
     """ Handle auction creation """
     parser = reqparse.RequestParser()
@@ -27,6 +41,7 @@ class AuctionList(Resource):
     auction.end_time = args.end_time
     auction.reserve = args.reserve
     auction.start_price = args.start_price
+    auction.creator = g.user
 
     crud.add(auction)
 
