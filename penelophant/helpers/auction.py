fdb82ff75450a6dc59c73cc5ec7c05f84f2cf856
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-04-02 02:43:26 -0400

    Add auction lookup helper

--- penelophant/helpers/auction.py
+++ penelophant/helpers/auction.py
@@ -1,6 +1,9 @@
 """ Auction helper functions """
 from penelophant.models import Auction
 from penelophant.models import auctions
+from penelophant.database import db
+from penelophant.models.Auction import Auction
+from flask_restful import abort
 
 def find_subclasses(module, clazz):
   """ Return all the classes in a given module """
@@ -18,3 +21,17 @@ def find_auction_type(typename):
     if clazz.__type__ == typename:
       return clazz
   return None
+
+def get_auction_by_id(auction_id):
+  """ Get a auction by a given id """
+  auction_id = int(auction_id)
+  session = db.session
+  auction = session.query(Auction).get(auction_id)
+  return auction
+
+def get_auction_by_id_or_abort(auction_id):
+  """ Attempt to get auction by id, or abort if it goes south """
+  auction = get_auction_by_id(auction_id)
+  if not auction:
+    abort(404, message="Auction {} doesn't exist".format(auction))
+  return auction
