3e61943d6efc2ed8b4ce9efb8f0b5066373512b6
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-04-09 00:37:03 -0400

    Properly support heroku postgres

--- penelophant/config/EnvConfig.py
+++ penelophant/config/EnvConfig.py
@@ -6,7 +6,7 @@ from .DefaultConfig import DefaultConfig
 class EnvConfig(DefaultConfig):
   """ Encapsulated configuration settings """
   DEBUG = os.environ.get('PENELOPHANT_DEBUG', False)
-  SQLALCHEMY_DATABASE_URI = os.environ.get('PENELOPHANT_SQLALCHEMY_DATABASE_URI')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
   SECRET_KEY = os.environ.get('PENELOPHANT_SECRET_KEY')
   AUTH_BACKENDS = []
   if os.environ.get('PENELOPHANT_AUTH_BACKENDS'):
