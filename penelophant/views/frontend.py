b87da18f2cdb95dfb1ee8ebc367e2162e32609e1
Kevin O'Connor <kevin.oconnor7@gmail.com>
2014-04-23 05:00:39 -0400

    Add frontend for heroku deployment

--- penelophant/views/frontend.py
+++ penelophant/views/frontend.py
@@ -1,10 +1,17 @@
 """ Frontend Views """
 
 from flask import Module
+from penelophant import app
 
 frontend = Module(__name__)
 
 @frontend.route('/')
 def index():
-  """ Place holder index page """
-  return "Hello World!"
+  """ Serve index page """
+  return app.send_static_file('index.html')
+
+
+@frontend.route('/<path:path>')
+def static_proxy(path):
+  # send_static_file will guess the correct MIME type
+  return app.send_static_file(path)
